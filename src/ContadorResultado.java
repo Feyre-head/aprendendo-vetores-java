c154557d684ef8978bff7cbbb259b9e21c5f3756
Feyre_head <inaraoenning@gmail.com>
2025-09-03 17:55:47 -0300

    Add: Loop for para armazenar cada resultado vitoria,derrota,empate / soma total de partidas e resultado total e individual.

--- src/ContadorResultado.java
+++ src/ContadorResultado.java
@@ -6,6 +6,10 @@ public class ContadorResultado {
         Scanner sc = new Scanner(System.in);
 
         int[] resultadoJogos = new int[10];
+        int vitorias = 0;
+        int derrotas = 0;
+        int empate = 0;
+
 
         System.out.println("Vamos anotar o resutado dos jogos");
 
@@ -16,7 +20,7 @@ public class ContadorResultado {
                     1- Vitória
                     2- Derrota
                     3- Empate
-                    : 
+                    :
                     """);
             int placar = sc.nextInt();
 
@@ -24,7 +28,21 @@ public class ContadorResultado {
         }
 
         for (int i = 0; i < 5; i++) {
-            System.out.println("Resultado do jogo " + (i + 1) + " : " + resultadoJogos[i]);
+
+            if (resultadoJogos[i] == 1){
+                vitorias++;
+            } else if (resultadoJogos[i] == 2) {
+                derrotas++;
+            } else {
+                empate++;
+            }
         }
+        int somaTotalDeJogos = vitorias + derrotas + empate;
+
+        System.out.println("========Resultado dos Jogos=======");
+        System.out.println("Total de Partidas:" + somaTotalDeJogos);
+        System.out.println("Vitórias: "+ vitorias);
+        System.out.println("Derrotas: " + derrotas);
+        System.out.println("Empate: " + empate);
     }
 }
